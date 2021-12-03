5c1d869f69f0e9dcd3faa6e72c85fd7fe2262ee9
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2021-12-03 20:44:40 +0100

    Day 2 Part 2

--- dec_02/src/main.rs
+++ dec_02/src/main.rs
@@ -1,31 +1,26 @@
 use std::io::{self, BufRead};
 
-struct Command {
-    position: i32,
+enum Command {
+    Forward(i32),
+    Down(i32),
+    Up(i32),
+}
+
+struct Submarine {
+    pos_x: i32,
     depth: i32,
+    aim: i32,
 }
 
 fn parse_command(line: &str) -> Command {
     let mut tokens = line.split(' ');
     let command = tokens.next().unwrap();
-    let value: i32 = tokens.next().unwrap().parse().unwrap();
+    let x: i32 = tokens.next().unwrap().parse().unwrap();
     match command {
-        "forward" => Command {
-            position: value,
-            depth: 0,
-        },
-        "down" => Command {
-            position: 0,
-            depth: value,
-        },
-        "up" => Command {
-            position: 0,
-            depth: -value,
-        },
-        _ => Command {
-            position: 0,
-            depth: 0,
-        },
+        "forward" => Command::Forward(x),
+        "down" => Command::Down(x),
+        "up" => Command::Up(x),
+        _ => Command::Forward(0),
     }
 }
 
@@ -35,14 +30,26 @@ fn main() {
         .lines()
         .map(|line| parse_command(line.unwrap().as_str()))
         .fold(
-            Command {
-                position: 0,
+            Submarine {
+                pos_x: 0,
                 depth: 0,
+                aim: 0,
             },
-            |a, b| Command {
-                position: a.position + b.position,
-                depth: a.depth + b.depth,
+            |accum, command| match command {
+                Command::Down(x) => Submarine {
+                    aim: accum.aim + x,
+                    ..accum
+                },
+                Command::Up(x) => Submarine {
+                    aim: accum.aim - x,
+                    ..accum
+                },
+                Command::Forward(x) => Submarine {
+                    pos_x: accum.pos_x + x,
+                    depth: accum.depth + accum.aim * x,
+                    aim: accum.aim,
+                },
             },
         );
-    println!("{}", result.position * result.depth);
+    println!("{}", result.pos_x * result.depth);
 }
