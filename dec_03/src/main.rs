1282b2dbcfd7318ca048af0f628c2b87d17b7de0
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2021-12-07 20:35:05 +0100

    Day 3 Part 2

--- dec_03/src/main.rs
+++ dec_03/src/main.rs
@@ -1,33 +1,44 @@
 use std::io::BufRead;
 
 fn main() {
-    let bit_counts: Vec<(i32, i32)> =
-        std::io::stdin()
-            .lock()
-            .lines()
-            .fold(vec![], |mut acc, line| {
-                for (i, c) in line.unwrap().chars().rev().enumerate() {
-                    if i >= acc.len() {
-                        acc.push((0, 0));
-                    }
-                    if c == '0' {
-                        acc[i].0 += 1
-                    } else {
-                        acc[i].1 += 1
-                    }
-                }
-                acc
-            });
+    let nums: Vec<usize> = std::io::stdin()
+        .lock()
+        .lines()
+        .map(|line| usize::from_str_radix(&line.unwrap(), 2).unwrap())
+        .collect();
 
-    let gamma: usize = bit_counts
-        .iter()
-        .enumerate()
-        .map(|(bit, counts)| ((counts.0 < counts.1) as usize) << bit)
-        .sum();
-    let epsilon: usize = bit_counts
-        .iter()
-        .enumerate()
-        .map(|(bit, counts)| ((counts.0 > counts.1) as usize) << bit)
-        .sum();
-    println!("{}", gamma * epsilon);
+    let mut oxy_mask = 0usize;
+    let mut co2_mask = 0usize;
+
+    for i in (0..usize::BITS).rev() {
+        let oxy_acc = nums
+            .iter()
+            .filter(|&num| {
+                num.checked_shr(i + 1).unwrap_or(0) == oxy_mask.checked_shr(i + 1).unwrap_or(0)
+            })
+            .map(|num| (num & (1 << i)) >> i)
+            .fold([0; 2], |mut accum, i| {
+                accum[i] += 1;
+                accum
+            });
+        oxy_mask |= ((oxy_acc[0] <= oxy_acc[1]) as usize) << i;
+
+        let co2_acc = nums
+            .iter()
+            .filter(|&num| {
+                num.checked_shr(i + 1).unwrap_or(0) == co2_mask.checked_shr(i + 1).unwrap_or(0)
+            })
+            .map(|num| (num & (1 << i)) >> i)
+            .fold([0; 2], |mut accum, i| {
+                accum[i] += 1;
+                accum
+            });
+        let mut argmin = (co2_acc[0] > co2_acc[1]) as usize;
+        if co2_acc[argmin] == 0 {
+            argmin = 1 - argmin;
+        }
+        co2_mask |= argmin << i;
+    }
+
+    println!("{} * {} = {}", oxy_mask, co2_mask, oxy_mask * co2_mask);
 }
