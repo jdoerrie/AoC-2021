bc67b3bb4f04d5bb38838d096942463183a18071
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2021-12-13 12:54:30 +0100

    Day 13

---
+++ dec_13/src/main.rs
@@ -0,0 +1,54 @@
+use std::collections::HashSet;
+use std::io::BufRead;
+
+enum Ins {
+    Point(usize, usize),
+    FoldX(usize),
+    FoldY(usize),
+}
+
+fn parse_line(line: &str) -> Option<Ins> {
+    if let Some((lhs, rhs)) = line.split_once(',') {
+        return Some(Ins::Point(lhs.parse().unwrap(), rhs.parse().unwrap()));
+    }
+
+    match line.split_once('=') {
+        Some((lhs, rhs)) => match lhs.chars().last().unwrap() {
+            'x' => Some(Ins::FoldX(rhs.parse().unwrap())),
+            'y' => Some(Ins::FoldY(rhs.parse().unwrap())),
+            _ => None,
+        },
+        _ => None,
+    }
+}
+
+fn main() {
+    let mut points = HashSet::new();
+    for ins in std::io::stdin()
+        .lock()
+        .lines()
+        .filter_map(|line| parse_line(&line.unwrap()))
+    {
+        match ins {
+            Ins::Point(x, y) => {
+                points.insert((x, y));
+            }
+            Ins::FoldX(l) => {
+                points = points
+                    .iter()
+                    .map(|&(x, y)| (if x < l { x } else { 2 * l - x }, y))
+                    .collect();
+                break;
+            }
+            Ins::FoldY(l) => {
+                points = points
+                    .iter()
+                    .map(|&(x, y)| (x, if y < l { y } else { 2 * l - y }))
+                    .collect();
+                break;
+            }
+        }
+    }
+
+    println!("{}", points.len());
+}
